b2820ec981e59756e90f1070505a6a0407c4f54f
Jacob Gadikian <jake@kloudery.com>
2016-06-20 05:22:06 +0700

    Using a pre-existing node to bootstrap, this now forms a consul cluster.  We'll see about Nomad momentarily.

--- scripts/consul.sh
+++ scripts/consul.sh
@@ -1,65 +1,28 @@
-#!/bin/sh
-#UPDATING UBUNTU AND INSTALLING DEPENDENCIES
+#!/bin/bash
+#UPDATING UBUNTU AND INSTALLING DEPENDENCIES.  Note that moving the loaders for consul-onboot.service and the consul-onboot script towards the end of this file will result in its abject failure.  I cannot for the life of me figure out why this is.  
+
 sudo -s
+cd /root
+sudo wget -O /etc/systemd/system/consul-onboot.service https://gist.githubusercontent.com/faddat/d143c53cbebba481f9159623b4472d1a/raw/fab65c4166428055577a56a9d96d23271bb6b08b/consul-onboot.service
+sudo wget -O /usr/bin/consul-onboot https://gist.githubusercontent.com/faddat/d143c53cbebba481f9159623b4472d1a/raw/fab65c4166428055577a56a9d96d23271bb6b08b/consul-onboot
+sudo apt install -y unzip moreutils build-essential 
 wget get.docker.io
 sh index.html
 wget https://releases.hashicorp.com/consul/0.6.4/consul_0.6.4_linux_amd64.zip
 unzip consul_0.6.4_linux_amd64.zip
-mv consul /usr/bin
-mkdir /data
+sudo mv consul /usr/bin/consul
+sudo gsutil cp gs://nomad/nomad /usr/bin/nomad
+chmod a+x /usr/bin/nomad
+sudo mkdir /data
 
 #DOWNLOADING NETWORK COMPONENTS
 sudo curl -L git.io/weave -o /usr/local/bin/weave
 sudo wget -O /usr/local/bin/scope https://git.io/scope
-sudo wget https://download.zerotier.com/dist/zerotier-one_1.1.4_amd64.deb
 
 #MARKING NETWORK COMPONENTS RUNNABLE
 sudo chmod a+x /usr/local/bin/weave
 sudo chmod a+x /usr/local/bin/scope
 
-#INSTALLING ZEROTIER
-sudo dpkg -i zerotier-one_1.1.4_amd64.deb
-
-#ZEROTIER-CLI BASH SCRIPT WITH FIVE SECOND DELAY BEFORE AND AFTER
-cat <<EOF >/usr/bin/zerotier;
-sleep 5s
-nohup zerotier-cli join 565799d8f6d1ae56 &
-sleep 5s
-EOF
-chmod a+x /usr/bin/zerotier
-
-#ONBOOT SYSTEMD UNIT FILE, RUNS KLOUDS' ONBOOT SCRIPT, WHICH CONNECTS THE VM TO KLOUDS' NETWORK
-cat <<EOF >/etc/systemd/system/consul-onboot.service;
-[Unit]
-Description=start klouds stack
-After=docker.service
-After=network-online.target
-After=zerotier.service
-Requires=zerotier-one.service
-Requires=docker.service
-[Service]
-ExecStart=/usr/bin/consul-onboot
-[Install]
-WantedBy=multi-user.target
-EOF
-
-#ONBOOT SCRIPT, which will start weave, scope and consul
-cat <<"EOF" >/usr/bin/consul-onboot;
-#!/bin/sh
-systemctl restart zerotier
-nohup docker daemon -H $(ifdata -pa zt0):2375 --storage-driver=overlay &
-/usr/local/bin/weave launch
-weave expose > weave
-/usr/local/bin/scope launch
-export ZT0=$(ifdata -pa zt0)
-/usr/bin/consul agent -atlas faddat/chicken -server -bind=$ZT0 -advertise=$ZT0 -bootstrap-expect=3 -atlas-join -data-dir=/data -atlas-token=yfIkFrF1SUKn5g.atlasv1.9laHhtFl6uAFyNO6qlZxXknpYJKdmix84c66mNryQ8wUHg0fPxqQvpfwlC79WAz4eqc
-EOF
-chmod a+x /usr/bin/consul-onboot
-
-
-systemctl daemon-reload
-systemctl enable consul-onboot
-systemctl enable docker
-systemctl enable zerotier
-systemctl enable zerotier-one
-systemctl enable ssh
+sudo chmod a+x /usr/bin/consul-onboot
+sudo systemctl daemon-reload
+sudo systemctl enable consul-onboot
