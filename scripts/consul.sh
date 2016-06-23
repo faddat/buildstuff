2e890066975aa1c951785f26681a6a70193c3e89
Jacob Gadikian <jake@kloudery.com>
2016-06-23 16:21:49 +0700

    it all works again!

--- scripts/consul.sh
+++ scripts/consul.sh
@@ -2,14 +2,15 @@
 #UPDATING UBUNTU AND INSTALLING DEPENDENCIES.  Note that moving the loaders for consul-onboot.service and the consul-onboot script towards the end of this file will result in its abject failure.  I cannot for the life of me figure out why this is.  
 
 sudo -s
+sudo apt update
+sudo apt upgrade -y
 cd /root
-sudo wget -O /etc/systemd/system/consul-onboot.service https://gist.githubusercontent.com/faddat/d143c53cbebba481f9159623b4472d1a/raw/fab65c4166428055577a56a9d96d23271bb6b08b/consul-onboot.service
-sudo wget -O /etc/systemd/system/nomad-onboot.service https://gist.githubusercontent.com/faddat/d143c53cbebba481f9159623b4472d1a/raw/e3c9e957a627223b4534157db83d8a346bed17d5/nomad.service
-sudo wget -O /usr/bin/consul-onboot https://gist.githubusercontent.com/faddat/d143c53cbebba481f9159623b4472d1a/raw/fab65c4166428055577a56a9d96d23271bb6b08b/consul-onboot
-sudo wget -O /usr/bin/nomad-onboot https://gist.githubusercontent.com/faddat/d143c53cbebba481f9159623b4472d1a/raw/e3c9e957a627223b4534157db83d8a346bed17d5/nomad-onboot
+sudo wget -O /etc/systemd/system/consul-onboot.service https://gist.githubusercontent.com/faddat/d143c53cbebba481f9159623b4472d1a/raw/79d7f89862f44d00f9a424b91489b72c77d1bf78/consul-onboot.service
+sudo wget -O /etc/systemd/system/nomad-onboot.service https://gist.githubusercontent.com/faddat/d143c53cbebba481f9159623b4472d1a/raw/79d7f89862f44d00f9a424b91489b72c77d1bf78/nomad.service
+sudo wget -O /usr/bin/consul-onboot https://gist.githubusercontent.com/faddat/d143c53cbebba481f9159623b4472d1a/raw/79d7f89862f44d00f9a424b91489b72c77d1bf78/consul-onboot
+sudo wget -O /usr/bin/nomad-onboot https://gist.githubusercontent.com/faddat/d143c53cbebba481f9159623b4472d1a/raw/79d7f89862f44d00f9a424b91489b72c77d1bf78/nomad-onboot
 sudo apt install -y unzip moreutils build-essential
-wget get.docker.io
-sh index.html
+wget -qO- https://get.docker.com/ | sh
 wget https://releases.hashicorp.com/consul/0.6.4/consul_0.6.4_linux_amd64.zip
 unzip consul_0.6.4_linux_amd64.zip
 sudo mv consul /usr/bin/consul
