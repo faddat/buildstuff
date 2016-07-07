2454dcd956e02ae6105c0c228f722d378291ba7d
Jacob Gadikian <faddat@gmail.com>
2016-07-07 23:07:16 +0700

    tweaked the build script to test images automaticall

--- scripts/consul.sh
+++ scripts/consul.sh
@@ -1,25 +1,4 @@
 #!/bin/bash
-#UPDATING UBUNTU AND INSTALLING DEPENDENCIES.  Note that moving the loaders for consul-onboot.service and the consul-onboot script towards the end of this file will result in its abject failure.  I cannot for the life of me figure out why this is.  
-
-sudo -s
-sudo apt update
-sudo apt upgrade -y
-cd /root
-sudo cp /tmp/serverfiles/consul-onboot /usr/bin/consul-onboot
-sudo cp /tmp/serverfiles/consul-onboot.service /etc/systemd/system/consul-onboot.service
-sudo cp /tmp/serverfiles/nomad-onboot /usr/bin/nomad-onboot
-sudo cp /tmp/serverfiles/nomad.service /etc/systemd/system/nomad.service
-sudo wget -O /usr/bin/discovery https://raw.githubusercontent.com/weaveworks/discovery/master/discovery
-sudo apt install -y unzip moreutils build-essential
-wget -qO- https://get.docker.com/ | sh
-wget https://releases.hashicorp.com/consul/0.6.4/consul_0.6.4_linux_amd64.zip
-unzip consul_0.6.4_linux_amd64.zip
-sudo mv consul /usr/bin/consul
-sudo gsutil cp gs://nomad/nomad /usr/bin/nomad
-sudo chmod a+x /usr/bin/nomad
-sudo chmod a+x /usr/bin/nomad-onboot
-sudo chmod a+x /usr/bin/consul-onboot
-sudo mkdir /data
 
 #DOWNLOADING NETWORK COMPONENTS
 sudo curl -L git.io/weave -o /usr/local/bin/weave
@@ -27,7 +6,31 @@ sudo wget -O /usr/local/bin/scope https://git.io/scope
 sudo chmod a+x /usr/local/bin/weave
 sudo chmod a+x /usr/local/bin/scope
 
+#UPDATING UBUNTU AND INSTALLING DEPENDENCIES.  Note that moving the loaders for consul-onboot.service and the consul-onboot script towards the end of this file will result in its abject failure.  I cannot for the life of me figure out why this is.
+sudo -s
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y unzip moreutils build-essential
+wget -qO- https://test.docker.com/ | sh
+cd /root
+sudo cp /tmp/serverfiles/scope.service /etc/systemd/system/scope.service
+sudo cp /tmp/serverfiles/weave.service /etc/systemd/system/weave.service
+sudo cp /tmp/serverfiles/consul.service /etc/systemd/system/consul.service
+sudo cp /tmp/serverfiles/nomad.service /etc/systemd/system/nomad.service
+wget https://releases.hashicorp.com/consul/0.6.4/consul_0.6.4_linux_amd64.zip
+wget https://releases.hashicorp.com/nomad/0.4.0/nomad_0.4.0_linux_amd64.zip
+unzip nomad_0.4.0_linux_amd64.zip
+unzip consul_0.6.4_linux_amd64.zip
+sudo mv consul /usr/bin/consul
+sudo mv nomad /usr/bin/nomad
+sudo chmod a+x /usr/bin/nomad
+sudo chmod a+x /usr/bin/consul
+sudo mkdir /data
+
+
 #Setting systemd Units as active
 sudo systemctl daemon-reload
-sudo systemctl enable consul-onboot
+sudo systemctl enable consul
 sudo systemctl enable nomad
+sudo systemctl enable weave
+sudo systemctl enable scope
--- three.sh
+++ three.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
-packer build -except virtualbox-ovf consul.json
-packer build -except qemu leaf.sh
-packer build -except virtualbox-ovf trunk.json
+packer build -except virtualbox-ovf consul.json > build.log
+export IMAGENAME=$(awk 'END {print $NF}' build.log)
+sleep 30
+gcloud compute --project "ancient-torch-134323" instance-templates 
+create "consultest" --machine-type "n1-highcpu-4" --network "default" 
+--maintenance-policy "MIGRATE" --scopes 
+default="https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring.write","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management" 
+--tags "http-server","https-server" --image 
+"/ancient-torch-134323/$IMAGENAME" --boot-disk-size "20" 
+--boot-disk-type "pd-standard" --boot-disk-device-name "consultest"
+
